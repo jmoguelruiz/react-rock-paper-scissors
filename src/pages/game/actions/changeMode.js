eb9f6785d500e7f85f6a8ddba1db61d2fb68893f
Jose Obed Moguel Ruiz <jo_obed@hotmail.com>
2019-02-10 17:15:36 -0600

    Se corrige error cuando se desconectaba el usuario

--- src/pages/game/actions/changeMode.js
+++ src/pages/game/actions/changeMode.js
@@ -2,27 +2,26 @@ import {CHANGE_MODE} from '../actionTypes';
 import resetBoard from './resetBoard';
 import {MODE_PLAYER_COMPUTER} from '../constants';
 import api from '../api';
-import {getMode} from '../selectors';
+import {getMode, getSocketId, getPlayerNumber} from '../selectors';
 
 export default function() {
     return (dispatch, getState) => {
 
         const state = getState();
         const modeActual = getMode(state);
-
-
-        if(modeActual == MODE_PLAYER_COMPUTER){
-            dispatch(api.serverConnectPlayer());
-        }else{
-            dispatch(api.serverDisconnectPlayer());
-        }
-
-        dispatch(resetBoard());
+        const socketId = getSocketId(state);
+        const playerNumber = getPlayerNumber(state);
 
         dispatch({
             type : CHANGE_MODE,
             payload : ''
         });
+
+        if(modeActual == MODE_PLAYER_COMPUTER){
+            dispatch(api.serverConnectPlayer());
+        }else{
+            dispatch(api.serverDisconnectPlayer(playerNumber, socketId));
+        }
 
 
     }
--- src/pages/game/api/serverDisconnectPlayer.js
+++ src/pages/game/api/serverDisconnectPlayer.js
@@ -1,13 +1,8 @@
 import {SERVER_DISCONNECT_PLAYER} from '../actionTypes';
 import {getSocketId, getPlayerNumber} from '../selectors';
 
-export default function(){
+export default function(playerNumber, socketId){
     return (dispatch, getState) => {
-
-        const state = getState()
-        const socketId = getSocketId(state);
-        const playerNumber = getPlayerNumber(state);
-
         return dispatch({
             type: SERVER_DISCONNECT_PLAYER,
             payload: {
