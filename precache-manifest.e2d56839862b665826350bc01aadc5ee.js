61826072f66afc6ed867a53da84bbd926c92cb15
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2020-09-26 16:59:29 -0500

    Updates

---
+++ precache-manifest.e2d56839862b665826350bc01aadc5ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ca415962bf25802ff62",
+    "url": "/react-rock-paper-scissors/static/css/main.75298303.chunk.css"
+  },
+  {
+    "revision": "4ca415962bf25802ff62",
+    "url": "/react-rock-paper-scissors/static/js/main.4ca41596.chunk.js"
+  },
+  {
+    "revision": "afc8b95afa5e8851bfcd",
+    "url": "/react-rock-paper-scissors/static/js/1.afc8b95a.chunk.js"
+  },
+  {
+    "revision": "2a50b9be4f48e8bc01f8",
+    "url": "/react-rock-paper-scissors/static/js/runtime~main.2a50b9be.js"
+  },
+  {
+    "revision": "d220c05ae08b8e99e79f2bcc3332344a",
+    "url": "/react-rock-paper-scissors/index.html"
+  }
+];
